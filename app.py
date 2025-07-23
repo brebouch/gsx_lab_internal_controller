bd4bb7ce42bcb3e90f64eeee5c1a572f4fbaf2b7
Brennan Bouchard <brebouch@cisco.com>
2025-07-23 09:46:13 -0700

    Updated controller health check

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 import system_info
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3001", "http://198.18.5.179"])
+CORS(app, origins=["*"])
 
 
 DEBOUNCE_FAIL_THRESHOLD = 4  # Number of missed checks before "down"
